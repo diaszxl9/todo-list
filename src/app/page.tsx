2b706a615bb11e46d76157ff43258a6fc885649d
diaszxl9 <nandodiass516@gmail.com>
2026-09-19 13:40:45 -0300

    configurando componentes e card da lista de tarefas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import { Separator } from "../components/ui/separator";
+import { Button } from "../components/ui/button";
+import { Card, CardContent, CardHeader} from "../components/ui/card";
+import { Input } from "../components/ui/input";
+import { Plus, Info, Menu, Check } from 'lucide-react';
+import { Badge } from "../components/ui/badge";
+
+
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
-    </div>
+    <main className = "w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className= "w-lg ">
+        <CardHeader className = "flex gap-2">
+          <Input placeholder ="Adicionar tarefa" />
+          <Button className="cursor-pointer"> <Plus/> Cadastrar </Button >
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4"/>
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer"> <Menu/> Todas </Badge>
+            <Badge className="cursor-pointer"> <Info/> Não finalizadas </Badge>
+            <Badge className="cursor-pointer"> <Check/> Concluidas </Badge>
+          </div>
+        </CardContent>
+        
+        
+      </Card>
+    </main>
   );
 }
 export default Home;
