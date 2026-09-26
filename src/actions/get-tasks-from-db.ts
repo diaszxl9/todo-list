8731cbd3191135006e5c81ceca7673a3ec2fecdd
diaszxl9 <nandodiass516@gmail.com>
2026-09-26 13:27:13 -0300

    Refactor task management: add task creation and deletion functionality, integrate Toaster for notifications, and update dependencies

--- src/actions/get-tasks-from-db.ts
+++ src/actions/get-tasks-from-db.ts
@@ -3,10 +3,12 @@
 import {prisma} from "@/utils/prisma"
 
 export const getTask = async () => {
-    const tasks = await prisma.task.findMany()
-
-    if(!tasks) return
-
-    console.log(tasks)
-    return tasks
+    try {
+        const tasks = await prisma.task.findMany()
+            if(!tasks) return
+            console.log(tasks)
+        return tasks
+    }catch (error) {
+        throw error
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,35 +17,81 @@ import {
 } from "@/components/ui/alert-dialog"
 import EditTask from "@/components/edit-task";
 import {getTask} from "@/actions/get-tasks-from-db"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Task } from "@/generated/prisma/client"
+import { NewTask } from "@/actions/add-task"
+import { deleteTask } from "@/actions/delete-task"
+import { toast } from "sonner";
+
 
 
 
 const Home = () => {
 
   const [taskList, setTaskList] = useState<Task[]>([])
+  const [task, setTask] = useState<string>('')
 
   const handleGetTask = async () => {
-    const tasks = await getTask()
-
-    if(!tasks) return
-
-    setTaskList(tasks)
-
-    console.log(tasks)
+    try{
+      const tasks = await getTask()
+        if(!tasks) return
+        setTaskList(tasks)
+      }catch(error){
+        throw error
+      }
   }
 
-console.log(taskList)
+  const handleAddTask = async  () => {
+    try {
+      if(task.length === 0 || !task) {
+      return
+    }
+
+    const myNewTask = await NewTask(task)
+
+      if(!myNewTask) return
+      await handleGetTask()
+
+      toast.success("atividade adicionada com sucesso!")
+
+    }catch(error){
+      throw error
+    }
+  }
+
+  const handleDeleteTask = async (id: string) =>{
+
+    try{
+      if (!id) return
+
+      const deletedTask = await deleteTask(id)
+
+      if(!deletedTask) return
+      await handleGetTask()
+      toast.warning("atividade deletada com sucesso!")
+
+      return
+
+    }catch(error){
+      throw error
+    }
+    
+    
+  }
+
+
+  useEffect (() => {
+    handleGetTask()
+  }, [])
 
   return (
     <main className = "w-full h-screen bg-gray-100 flex justify-center items-center">
       <Card className= "w-lg ">
         <CardHeader className = "flex gap-2">
-          <Input placeholder ="Adicionar tarefa" />
-          <Button className="cursor-pointer"> <Plus/> Cadastrar </Button >
+          <Input placeholder ="Adicionar tarefa" onChange={(e)=> setTask(e.target.value) } />
+          <Button className="cursor-pointer" onClick={handleAddTask}> <Plus/> Cadastrar </Button >
         </CardHeader>
-        <Button onClick={handleGetTask}>Buscar Tarefas</Button>
+        
         <CardContent>
           <Separator className="mb-4"/>
           <div className="flex gap-2">
@@ -56,16 +102,18 @@ console.log(taskList)
 
           <div className= "mt-4 border b"> 
 
-            <div className ="h-14 flex justify-between items-center border-b border-t">
+           {taskList.map(task => (
+            <div className ="h-14 flex justify-between items-center border-b border-t" key = {task.id}>
               <div className = "w-1 h-full bg-green-300"></div>
-              <p className="flex-1 px-2 text-sm"> Estudar React </p>
+              <p className="flex-1 px-2 text-sm">{task.task}</p>
               <div className = "flex gap-2 items-center">
 
               <EditTask/>
           
-                 <Trash size ={16} className="cursor-pointer"/> 
+                 <Trash size ={16} className="cursor-pointer" onClick={() => handleDeleteTask(task.id)}/> 
               </div>
             </div>
+           ))} 
 
           </div>
           
